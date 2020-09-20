647b7efc218f27459fbd336bb7168e6924575e41
Antoninadan <mail100@i.ua>
2020-09-20 13:32:59 +0300

    continue statictics

--- src/statictics/task.sql
+++ src/statictics/task.sql
@@ -63,6 +63,8 @@ insert into addresses (city, street, house, apartment) values('Lviv', 'Lychakivs
 insert into addresses (city, street, house, apartment) values('Kharkiv', 'Shevchenko', 10, 1); -- 4
 insert into addresses (city, street, house, apartment) values('Kharkiv', 'Polyova', 100, 16); -- 5
 insert into addresses (city, street, house, apartment) values('Zhytomyr', 'Shevchenko', 9, 9); -- 6
+insert into addresses (city, street, house, apartment) values('Kyiv', 'Kashtanova', 13, 22); -- 7
+insert into addresses (city, street, house, apartment) values('Kharkiv', 'Polyova', 102, 13); -- 8
 
 insert into faculties (name, foundation_date) values('Fizyka', 'Jan-08-1990'); -- 1
 insert into faculties (name, foundation_date) values('Biology', 'Jun-06-2000'); -- 2
@@ -105,6 +107,20 @@ insert into groups (name, foundation_date, faculty_id) values('Math-3', 'Feb-1-1
 insert into groups (name, foundation_date, faculty_id) values('Math-4', 'Feb-1-1990', 5); -- 13  Math
 insert into groups (name, foundation_date, faculty_id) values('Kosmics-1', 'Feb-1-1990', 6); -- 14  Kosmics
 
+insert into students (first_name, last_name, sex, birthday, address_id, group_id) values('Ivan', 'Ivanov', true, 'Jan-18-1990', 1);
+insert into students (first_name, last_name, sex, birthday, address_id, group_id) values('Stepan', 'Stepanov', true, 'Jan-18-1990', 2);
+insert into students (first_name, last_name, sex, birthday, address_id, group_id) values('Kostyantyn', 'Kostyantynov', true, 'Jan-8-1991', 3);
+insert into students (first_name, last_name, sex, birthday, address_id, group_id) values('Danylo', 'Danylov', true, 'Feb-10-1999', 4);
+insert into students (first_name, last_name, sex, birthday, address_id, group_id) values('Nadia', 'Nadivko', false, 'Mar-1-1990', 5);
+insert into students (first_name, last_name, sex, birthday, address_id, group_id) values('Oleg', 'Oleghkiv', true, 'Mar-3-1990', 6);
+insert into students (first_name, last_name, sex, birthday, address_id, group_id) values('Maria', 'Mariyva', false, 'Feb-10-1999', 6);
+insert into students (first_name, last_name, sex, birthday, address_id, group_id) values('Maria', 'Mariyva', false, 'Apr-23-1998', 6);
+insert into students (first_name, last_name, sex, birthday, address_id, group_id) values('Larysa', 'Lasysko', false, 'Mar-8-1990', 6);
+insert into students (first_name, last_name, sex, birthday, address_id, group_id) values('Tymifiy', 'Oleghkiv', true, 'Feb-10-1999', 6);
+insert into students (first_name, last_name, sex, birthday, address_id, group_id) values('Kate', 'Katev', false, 'Apr-4-1998', 6);
+insert into students (first_name, last_name, sex, birthday, address_id, group_id) values('Nadia', 'Nadiyv', false, 'Jan-08-1990', 3);
+insert into students (first_name, last_name, sex, birthday, address_id, group_id) values('Danylo', 'Kryh', true, 'Apr-22-1998', 1);
+insert into students (first_name, last_name, sex, birthday, address_id, group_id) values('Danylo', 'Kuhar', true, 'Apr-1-1998', 6);
 insert into students (first_name, last_name, sex, birthday, address_id, group_id) values('Ivan', 'Ivanov', true, 'Jan-18-1990', 1, 1);
 insert into students (first_name, last_name, sex, birthday, address_id, group_id) values('Stepan', 'Stepanov', true, 'Jan-18-1990', 2, 1);
 insert into students (first_name, last_name, sex, birthday, address_id, group_id) values('Kostyantyn', 'Kostyantynov', true, 'Jan-8-1991', 3, 2);
@@ -119,3 +135,200 @@ insert into students (first_name, last_name, sex, birthday, address_id, group_id
 insert into students (first_name, last_name, sex, birthday, address_id, group_id) values('Nadia', 'Nadiyv', false, 'Jan-08-1990', 3, 6);
 insert into students (first_name, last_name, sex, birthday, address_id, group_id) values('Danylo', 'Kryh', true, 'Apr-22-1998', 1, 6);
 insert into students (first_name, last_name, sex, birthday, address_id, group_id) values('Danylo', 'Kuhar', true, 'Apr-1-1998', 6, 6);
+
+UPDATE students SET group_id = 14, first_name = 'Foma' WHERE id = 1;
+UPDATE students SET group_id = 13, first_name = 'Ivan' WHERE id = 2;
+UPDATE students SET group_id = 12, first_name = 'Den' WHERE id = 3;
+UPDATE students SET group_id = 11, first_name = 'Anna' WHERE id = 4;
+UPDATE students SET group_id = 10, first_name = 'Rose' WHERE id = 5;
+UPDATE students SET group_id = 9, first_name = 'Oleg' WHERE id = 6;
+UPDATE students SET group_id = 8, first_name = 'Ganna' WHERE id = 7;
+UPDATE students SET group_id = 7, first_name = 'Lyudmyla' WHERE id = 8;
+UPDATE students SET group_id = 6, first_name = 'Olena' WHERE id = 9;
+UPDATE students SET group_id = 14, first_name = 'Ivan' WHERE id = 10;
+UPDATE students SET group_id = 13, first_name = 'Olena' WHERE id = 11;
+UPDATE students SET group_id = 14, first_name = 'Inna' WHERE id = 12;
+UPDATE students SET group_id = 14, first_name = 'Gnat' WHERE id = 13;
+UPDATE students SET group_id = 8, first_name = 'Foma' WHERE id = 14;
+
+
+UPDATE groups SET monitor_id = 15 WHERE id = 1;
+UPDATE groups SET monitor_id = 17 WHERE id = 2;
+UPDATE groups SET monitor_id = 20 WHERE id = 3;
+UPDATE groups SET monitor_id = 21 WHERE id = 4;
+UPDATE groups SET monitor_id = 23 WHERE id = 5;
+UPDATE groups SET monitor_id = 27 WHERE id = 6;
+UPDATE groups SET monitor_id = 8 WHERE id = 7;
+UPDATE groups SET monitor_id = 7 WHERE id = 8;
+UPDATE groups SET monitor_id = 6 WHERE id = 9;
+UPDATE groups SET monitor_id = 5 WHERE id = 10;
+UPDATE groups SET monitor_id = 4 WHERE id = 11;
+UPDATE groups SET monitor_id = 3 WHERE id = 12;
+UPDATE groups SET monitor_id = 2 WHERE id = 13;
+UPDATE groups SET monitor_id = 1 WHERE id = 14;
+
+DELETE FROM students WHERE id in (22,24,25,26,28,9,10,16);
+
+UPDATE students SET address_id = 7 WHERE id in (20, 6, 14);
+UPDATE students SET address_id = 8 WHERE id in (23, 11);
+
+-- university, faculty, group
+select u.name as university, f.name as faculty, g.name as group
+from groups g
+join faculties f on f.id = g.faculty_id
+join faculties_universities fu on fu.faculty_id = f.id
+join universities u on u.id = fu.university_id
+order by university, faculty
+
+--всех студентов учащихся в одном университете(по названию университета).
+select u.name as university, s.id as student_id, s.first_name, s.last_name
+from students s
+join groups g on g.id = s.group_id
+join faculties f on f.id = g.faculty_id
+join faculties_universities fu on fu.faculty_id = f.id
+join universities u on u.id = fu.university_id and u.name = 'KPI' --'Kyiv-Mohyla academy'
+order by s.id
+
+--всех студентов учащихся в одном университете(по названию университета) и на одном факультете
+select u.name as university, s.id as student_id, s.first_name, s.last_name, g.name as group
+from students s
+join groups g on g.id = s.group_id
+join faculties f on f.id = g.faculty_id
+join faculties_universities fu on fu.faculty_id = f.id and f.name = 'Math'
+join universities u on u.id = fu.university_id and u.name = 'KPI'
+order by s.id
+
+
+-- количество студентов учащихся в каждом университете, факультете, группе.  (!) one faculty in diff university
+select u.name as university, f.name as faculty, g.name as group, count(*)
+from students s
+join groups g on g.id = s.group_id
+join faculties f on f.id = g.faculty_id
+join faculties_universities fu on fu.faculty_id = f.id
+join universities u on u.id = fu.university_id
+group by u.name, f.name, g.name
+order by u.name, f.name, g.name
+
+--найти максимальное/минимальное количество студентов в группе (розложено по университетам)
+SELECT Min(foo.count),
+       Max(foo.count)
+FROM   (SELECT u.name   AS university,
+               f.name   AS faculty,
+               g.name   AS group,
+               Count(*) AS count
+        FROM   students s
+               join GROUPS g ON g.id = s.group_id
+               join faculties f ON f.id = g.faculty_id
+               join faculties_universities fu ON fu.faculty_id = f.id
+               join universities u ON u.id = fu.university_id
+        GROUP  BY u.name, f.name, g.name
+        ORDER  BY u.name, f.name, g.name) foo
+
+--найти максимальное/минимальное количество студентов в группе
+(SELECT 'max',
+        g.name   AS group,
+        Count(*) AS count
+ FROM   students s
+        JOIN groups g ON g.id = s.group_id
+ GROUP  BY g.name
+ ORDER  BY Count(*) DESC
+ LIMIT  1)
+UNION ALL
+(SELECT 'min',
+        g.name   AS group,
+        Count(*) AS count
+ FROM   students s
+        JOIN groups g ON g.id = s.group_id
+ GROUP  BY g.name
+ ORDER  BY Count(*)
+ LIMIT  1)
+
+--подсчитать количество студентов, которые живут по одному адресу(улица, дом)
+SELECT adr2.street, adr2.house, adr1.count
+FROM   (SELECT a.id     AS address_id,
+               Count(*) AS count
+        FROM   addresses a
+               JOIN students s ON s.address_id = a.id
+        GROUP  BY a.id) adr1
+        JOIN addresses adr2 ON adr2.id = adr1.address_id
+ORDER  BY adr2.street, adr2.house, adr1.count
+
+--вывести даты рождения студентов в формате '30 Oct 16'
+select TO_CHAR(birthday :: DATE, 'dd Mon yy')
+from students
+
+--вывести среднее количество людей в группе по университету
+SELECT foo.university, Avg(foo.count)
+FROM   (SELECT u.name   AS university,
+               g.name   AS group,
+               Count(*) AS count
+        FROM   students s
+               join GROUPS g ON g.id = s.group_id
+               join faculties f ON f.id = g.faculty_id
+               join faculties_universities fu ON fu.faculty_id = f.id
+               join universities u ON u.id = fu.university_id --and u.name = 'KPI'
+        GROUP  BY u.name, g.name
+        ORDER  BY u.name, g.name) foo
+GROUP  BY foo.university
+
+--вывести всех старост групп
+select g.name, s.first_name, s.last_name
+from groups g
+join students s on s.id = g.monitor_id
+
+-- вывести университет, с наибольшим количеством факультетов, групп, студентов.
+(SELECT 'max group number', ugc.university_name, Max(ugc.university_group_number) as maximum
+FROM   (SELECT gc.university          AS university_name,
+               Count(gc.group_number) AS university_group_number
+        FROM   (SELECT u.name   AS university,
+                       g.name   AS GROUP,
+                       Count(*) AS group_number
+                FROM   GROUPS g
+                       join faculties f
+                         ON f.id = g.faculty_id
+                       join faculties_universities fu
+                         ON fu.faculty_id = f.id
+                       join universities u
+                         ON u.id = fu.university_id
+                GROUP  BY u.name,
+                          g.name) gc
+        GROUP  BY gc.university) ugc
+GROUP  BY ugc.university_name
+order by 2 desc
+limit 1)
+union
+(SELECT 'max faculty number', ufc.university_name, Max(ufc.university_faculty_number) as maximum
+FROM   (SELECT fc.university          AS university_name,
+               Count(fc.faculty_number) AS university_faculty_number
+        FROM   (SELECT u.name   AS university,
+                       f.name as faculty,
+                       Count(*) AS faculty_number
+                FROM   faculties f
+                       join faculties_universities fu
+                         ON fu.faculty_id = f.id
+                       join universities u
+                         ON u.id = fu.university_id
+                GROUP  BY u.name,
+                          f.name) fc
+        GROUP  BY fc.university) ufc
+GROUP  BY ufc.university_name
+order by 2 desc
+limit 1)
+union
+(SELECT 'max student number', usc.university_name, Max(usc.university_student_number) as maximum
+FROM   (SELECT sc.university          AS university_name,
+               Count(sc.student_number) AS university_student_number
+        FROM   (SELECT u.name   AS university,
+                       s.id   AS student,
+                       Count(*) AS student_number
+                FROM   students s
+				       join groups g ON g.id = s.group_id
+                       join faculties f ON f.id = g.faculty_id
+                       join faculties_universities fu ON fu.faculty_id = f.id
+                       join universities u ON u.id = fu.university_id
+                GROUP  BY u.name,
+                          s.id) sc
+        GROUP  BY sc.university) usc
+GROUP  BY usc.university_name
+order by 2 desc
+limit 1)
